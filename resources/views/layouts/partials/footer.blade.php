45a80c5766b64527ca3b77b32d5af4559299a616
KenethUrrutia <106717176+KenethUrrutia@users.noreply.github.com>
2023-09-28 22:45:08 -0600

    Update footer

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -3,14 +3,15 @@
         <div class="responsables">
             <p>Responsables:</p>
             <ul>
+                <li>Yuliana Oliva</li>
                 <li>Keneth Urrutia</li>
                 <li>Josmar Requena</li>
-                <li>Yuliana Oliva</li>
                 <li>Jeymi</li>
                 <li>Carlos</li>
             </ul>
         </div>
         <div class="info">
+            <br><br><br><br><br>
             <p>CUNORI - USAC &copy;2023</p>
             <p>Proyecto de Diplomado de Emprendimiento Innovador</p>
         </div>

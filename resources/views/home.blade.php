fa7d598fbbe3140a424eddcc70534a26377c2143
Xtremer94 <rundas94@outlook.com>
2023-10-03 02:36:19 -0600

    cambio de nombre de emprendimiento

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,9 @@
 
 <div style="margin-top: 5%; margin-left: auto; margin-right: auto; width: 50%; text-align: center;">
     <img src="{{ asset('MMEB.ico') }}" alt="" style="width: 40%">
-    <h1>Manage My E-Business</h1>
+    <h1>Digi-solutions</h1>
 </div>
-    
+
 
 
 @endsection

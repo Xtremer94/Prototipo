ca7be463ea777b143d1abca20eed5be34f75d493
Xtremer94 <rundas94@outlook.com>
2023-10-03 03:05:45 -0600

    cambio de logo y titulos del emprendimiento

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <div style="margin-top: 5%; margin-left: auto; margin-right: auto; width: 50%; text-align: center;">
-    <img src="{{ asset('MMEB.ico') }}" alt="" style="width: 40%">
+    <img src="{{ asset('DGSL.png') }}" alt="" style="width: 40%">
     <h1>Digi-solutions</h1>
 </div>
 
